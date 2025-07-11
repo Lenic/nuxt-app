b749686efffa7626cd0e725772819d5493226116
Lenic Lei <lenic_lei@manka.co.jp>
2025-07-11 09:40:36 +0900

    add prettier and tailwindcss.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
+import tailwindcss from '@tailwindcss/vite';
+import { eventHandler } from 'h3';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/eslint']
-})
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/eslint'],
+  css: ['~/assets/styles/global.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devServerHandlers: [
+    {
+      route: '/.well-known/appspecific',
+      handler: eventHandler(() => new Response(null, { status: 204 })),
+    },
+  ],
+});
